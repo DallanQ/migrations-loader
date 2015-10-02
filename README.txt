37c703f2f3cfac321e9deeff3bc10d15a5b34769
Dallan Quass <dallan@quass.org>
2015-10-02 10:09:31 -0600

    starting over

--- README.txt
+++ README.txt
@@ -1,19 +1,3 @@
-REFRESH DEPENDENCIES - last refreshed 10 Sep 2015
+REFRESH DEPENDENCIES - last refreshed 2 Oct 2015
 
-git subtree pull --prefix src/github.com/golang/protobuf https://github.com/golang/protobuf master --squash
-git subtree pull --prefix src/golang.org/x/net https://github.com/golang/net master --squash
-git subtree pull --prefix src/golang.org/x/oauth2 https://github.com/golang/oauth2 master --squash
-git subtree pull --prefix src/google.golang.org/grpc https://github.com/grpc/grpc-go master --squash
-git subtree pull --prefix src/github.com/bradfitz/http2 https://github.com/bradfitz/http2 master --squash
-git subtree pull --prefix src/google.golang.org/api https://github.com/google/google-api-go-client master --squash
-git subtree pull --prefix src/google.golang.org/cloud https://github.com/GoogleCloudPlatform/gcloud-golang master --squash
-
-CHANGES
-
-git rm -rf src/google.golang.org/cloud/examples
-git rm -rf src/google.golang.org/api/examples
-git rm -rf src/google.golang.org/grpc/examples
-
-DON'T FORGET
-
-gcloud config set project fsmigrations
+git subtree pull --prefix src/github.com/go-sql-driver/mysql https://github.com/go-sql-driver/mysql master --squash
--- src/github.com/DallanQ/migrations-loader/main.go
+++ src/github.com/DallanQ/migrations-loader/main.go
@@ -1,57 +1,12 @@
 package main
 
-import (
-	"flag"
-	"fmt"
-	"os"
-    "google.golang.org/cloud/datastore"
-    "log"
-)
-
-const projectID = "fsmigrations"
-const keyFilepath = "resources/fsmigrations-f4d1a46ae501.json"
-const dbKind = "migrations"
-const namespace = "test"
-
-var typ = flag.String("t", "", "(i)mmigrations or (e)migrations (required)")
-
-type Migration struct {
-	Type   string     `json:"type" datastore:",noindex"`
-	Place  string     `json:"place" datastore:",noindex"`
-	Year   string     `json:"year" datastore:",noindex"`
-}
-
 func main() {
-	flag.Parse()
-	if *typ != "i" && *typ != "e" {
-		flag.Usage()
-		os.Exit(1)
-	}
+    flag.Parse()
+   	if *typ != "i" && *typ != "e" {
+   		flag.Usage()
+   		os.Exit(1)
+   	}
 
-	fmt.Println("Hello world5")
+   	fmt.Println("Hello world fs2")
 
-    ctx := datastore.WithNamespace(getContext(projectID, keyFilepath, datastore.ScopeDatastore, datastore.ScopeUserEmail), namespace)
-    client, err := datastore.NewClient(ctx, projectID)
-    if err != nil {
-        log.Fatal("Error getting client", client)
-    }
-
-    key := datastore.NewKey(ctx, "testkind", "test2", 0, nil)
-    src := &Migration{
-        Type: "e",
-        Place: "place",
-        Year: "1800",
-    }
-
-    fmt.Println("before retry")
-
-    retry(func() error {
-        fmt.Println("inside retry")
-   		foo, err := client.Put(ctx, key, src)
-        fmt.Println("Put", foo, err)
-   		return err
-   	})
-
-    fmt.Println("Goodbye world")
 }
-
